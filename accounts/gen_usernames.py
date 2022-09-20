903bd1e90824ca684e00609db12a5e6b2b5b4e75
exploide <me@exploide.net>
2022-09-20 18:31:53 +0200

    gen_usernames.py: linter fixes

--- accounts/gen_usernames.py
+++ accounts/gen_usernames.py
@@ -10,15 +10,15 @@ import argparse
 
 def main(names_file):
     names = None
-    with open(names_file, 'r') as f:
+    with open(names_file, 'r', encoding='utf-8') as f:
         names = f.readlines()
     names = [n.split() for n in names if n.strip() != ""]
 
-    with open("first.last.txt", 'w') as f:
+    with open("first.last.txt", 'w', encoding='utf-8') as f:
         for name in names:
             f.write('.'.join([n.lower() for n in name]) + '\n')
 
-    with open("flast.txt", 'w') as f:
+    with open("flast.txt", 'w', encoding='utf-8') as f:
         for name in names:
             if len(name) > 1:
                 f.write(name[0][0].lower() + name[-1].lower() + '\n')
