19476401008fc9946fd6e955dcc13185b9c94043
exploide <me@exploide.net>
2023-05-01 20:47:09 +0200

    wifi/probe_request_sniffer.py: modernized script
    The script was broken, mainly due to the dependency on the deprecated iwlist command.
    Using iw instead for now.
    Furthermore, applied some restructuring and linter fixes.

--- wifi/probe_request_sniffer.py
+++ wifi/probe_request_sniffer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 """
-Tool to capture 802.11 Probe Request frames and print them.
+Tool to capture IEEE 802.11 Probe Request frames and print them.
 
 Useful in a demonstration to show known WiFi networks of visitors.
 """
@@ -21,32 +21,42 @@ def signal_handler(signal, frame):
     sys.exit(0)
 
 
+def channel_list(device):
+    iw_dev_info = subprocess.run(["iw", "dev", device, "info"], check=True, capture_output=True, text=True).stdout
+    phy = re.findall(r"\s*wiphy (\d+)\s*", iw_dev_info)[0]
+    iw_phy_channels = subprocess.run(["iw", "phy", f"phy{phy}", "channels"], check=True, capture_output=True, text=True).stdout
+    return re.findall(r"\[(\d+)\] \n", iw_phy_channels)
+
+
 def packet_handler(pkt):
     source = pkt.addr2
     ssid = pkt.info.decode()
     if ssid:
         if hasattr(packet_handler, "recent_probes"):
-            if (source, ssid) in packet_handler.recent_probes:
+            if ssid in packet_handler.recent_probes:
                 return
-            else:
-                packet_handler.recent_probes.append((source, ssid))
-        print("Probe Request from {} for SSID {}".format(source, ssid))
+            packet_handler.recent_probes.append(ssid)
+        print(f"Probe Request from {source} for SSID {ssid}")
 
 
-def main(args):
-    signal.signal(signal.SIGINT, signal_handler)
-    if args.throttle:
+def sniff_probe_requests(iface, throttle=False):
+    if throttle:
         packet_handler.recent_probes = collections.deque(maxlen=10)
-    iwlist_output = subprocess.run(["iwlist", args.iface, "freq"], stdout=subprocess.PIPE, encoding="utf-8").stdout
-    supported_channels = re.findall(r"Channel (\d+) :", iwlist_output)
-    for chan in itertools.cycle(supported_channels):
-        subprocess.run(["iw", "dev", args.iface, "set", "channel", chan])
-        sniff(iface=args.iface, timeout=3, prn=packet_handler, lfilter=lambda pkt: pkt.haslayer(Dot11ProbeReq))
+    for chan in itertools.cycle(channel_list(iface)):
+        subprocess.run(["iw", "dev", iface, "set", "channel", chan], check=True)
+        sniff(iface=iface, timeout=3, prn=packet_handler, lfilter=lambda pkt: pkt.haslayer(Dot11ProbeReq))
 
 
-if __name__ == '__main__':
-    argparser = argparse.ArgumentParser(description="Sniff for 802.11 Probe Request frames.")
+def main():
+    argparser = argparse.ArgumentParser(description="Sniff for IEEE 802.11 Probe Request frames.")
     argparser.add_argument("iface", help="the monitor mode enabled interface")
     argparser.add_argument("--throttle", action="store_true",
-                           help="throttle the occurrence of duplicate probe requests")
-    main(argparser.parse_args())
+                           help="throttle the occurrence of duplicate SSIDs")
+    args = argparser.parse_args()
+
+    signal.signal(signal.SIGINT, signal_handler)
+    sniff_probe_requests(args.iface, args.throttle)
+
+
+if __name__ == "__main__":
+    main()
